039266444a4654dec21948fda3b5d37578b755ca
Ron <Ron@gmail.com>
2021-06-08 19:17:03 +0300

    Frame

---
+++ loadbalancer.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+
+#define PORT 8080
+
+int main() {
+
+    
+    return 0;
+}
